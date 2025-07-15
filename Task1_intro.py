e4e4a65df1fafa50d5e9ba1fe75ab703931eb3a3
Supriya-2500 <supriya2500@gmail.com>
2025-07-16 01:57:30 +0530

    Add files via upload

---
+++ Task1_intro.py
@@ -0,0 +1,10 @@
+# Task 1 : Create your own Python script
+
+#Input
+
+name = input("Enter your name: ")
+fav_color = input("Favourite colour: ")
+query = input("What do you want to automate? ")
+
+#Output
+print(f"Hi {name}! Automating {query} in {fav_color} vibes? Let's do it!")
---
+++ day1_basics.py
@@ -0,0 +1,13 @@
+# Day 1 - Pythons Basics by Supriya
+
+#Input
+name = input("Enter your name:")
+
+#Variables
+age = 31
+is_learning = True
+
+#Output
+print(f"Hello {name}, welcome to your python journey!")
+print(f"Your current age is:{age}")
+print("Are you learning Python?", is_learning)
